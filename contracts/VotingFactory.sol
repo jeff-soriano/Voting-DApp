0bc346871b742755c063dac63a9f81e640887e3a
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-25 10:41:45 -0800

    Add comment documentation to VotingFactory

--- contracts/VotingFactory.sol
+++ contracts/VotingFactory.sol
@@ -9,6 +9,14 @@ import "./Voting.sol";
 contract VotingContract {
     address[] public votingContracts;
 
+    /// Creates a Voting contract
+    /// @param manager the address that will be managing the Voting contract
+    /// @param description the description of the Voting contract that tells the voters what they're voting for
+    /// @param optionADescription the description of optionA
+    /// @param optionBDescription the description of optionB
+    /// @param intendedVotingDate the intended date that the manager will move the contract to the Voting phase, in Unix time
+    /// @param intendedClosingDate the intended date that the manager will move the contract to the Closed phase, in Unix time
+    /// @dev this will create a Voting contract and then add its address to 'votingContracts'
     function createVotingContract(
         address manager,
         string memory description,
@@ -32,6 +40,8 @@ contract VotingContract {
         votingContracts.push(votingAddress);
     }
 
+    /// Get all the Voting contracts' addresses
+    /// @dev returns the 'votingContracts' array
     function getVotingContracts() public view returns (address[] memory) {
         return votingContracts;
     }
